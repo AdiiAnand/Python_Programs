c2dde405a02611d86840e33e996d0fc621d5e9e6
Aditya Anand <adityaanand430@gmail.com>
2023-08-23 07:25:24 +0530

    Day-4-Random Heads and Tails

---
+++ Day-4-Head and Tails/main.py
@@ -0,0 +1,11 @@
+# Remember to use the random module
+# Hint: Remember to import the random module here at the top of the file. 🎲
+
+# Write the rest of your code below this line 👇
+import random
+outcome = random.randint(0, 1)
+if outcome == 1:
+    print("Heads")
+else:
+    print("Tails")
+
